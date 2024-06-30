c70b1f4dc363ecf7ed5d57ed76a8f2a414ae10c1
krau5 <apvbd17@gmail.com>
2024-06-30 18:52:42 +0200

    Install Vuetify, create LinksList component, move links from public to assets

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import './style.css';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
