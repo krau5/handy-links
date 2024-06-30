59861a22dd66db91264b59f14f9da405f237a1d4
Vadym Kravchenko <apvbd17@gmail.com>
2024-06-30 18:53:15 +0200

    Install Vuetify, create LinksList component, move links from public to assets (#1)

---
+++ src/components/LinksList.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import links from '../assets/links.json';
+</script>
+
+<template>
+  <div>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          v-for="link in links"
+          :key="link.name"
+          cols="12"
+          sm="4"
+          md="3"
+          class="d-flex"
+        >
+          <v-card
+            :href="link.url"
+            target="_blank"
+            class="d-flex align-center justify-center text-center elevation-5 rounded-lg"
+            style="width: 100%; height: 100px;"
+          >
+            <v-card-text class="text-body-1">{{ link.name }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+.v-card {
+  transition: transform 0.2s;
+}
+.v-card:hover {
+  transform: scale(1.05);
+}
+</style>
+
+
