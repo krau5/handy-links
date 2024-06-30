326abc4572651ca01078bca6d2b1f1e6e1ac855c
krau5 <apvbd17@gmail.com>
2024-06-30 19:01:58 +0200

    Introduce ESLint & pre-commit hooks via Husky

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import LinksList from './components/LinksList.vue';
 </script>
 
 <template>
-  <h3 class="text-h3 font-weight-bold text-center mb-4">Handy Links</h3>
+  <h3 class="text-h3 font-weight-bold text-center mb-4">
+    Handy Links
+  </h3>
 
   <LinksList />
 </template>
